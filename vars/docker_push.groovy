f542ce6c9c7d6b32473251050b01f62fbaca0f15
manishvishwakarma89 <manish.kumar.v@ramanujan.du.ac.in>
2026-06-11 12:44:59 +0530

    Fix credential ID and quoting in docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,11 +1,11 @@
 def call(String imageName, String tag, String dockerHubUser){
     withCredentials([usernamePassword(
-        credentialsId: 'dockerhubcreds',
+        credentialsId: 'dockerHubCreds',
         usernameVariable: 'USER',
         passwordVariable: 'PASS'
     )]){
         sh """
-            echo ${PASS} | docker login -u ${USER} --password-stdin
+            echo '${PASS}' | docker login -u '${USER}' --password-stdin
             docker tag ${imageName}:${tag} ${dockerHubUser}/${imageName}:${tag}
             docker push ${dockerHubUser}/${imageName}:${tag}
         """
